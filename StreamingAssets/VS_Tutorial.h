44dd8d0eb0b2d9ea2e99a27a03af1bab7a7c91ba
bongame <149058735+bongame@users.noreply.github.com>
2025-05-01 10:12:39 +0900

    test

---
+++ StreamingAssets/VS_Tutorial.h
@@ -0,0 +1,26 @@
+﻿/*===========================================================================*
+ *  Header file for Atom CueSheet Binary
+ *  Project          : BuzzOrDie
+ *  Tool Ver.        : Ver.3.54.02
+ *  ACB  Ver.        : Ver.1.44.1
+ *  File Path        : C:/Users/s1len/OneDrive/Documents/CRIWARE/CriAtomCraft/projects/BuzzOrDie/Public/BGM
+ *  File Name        : VS_Tutorial.h
+ *  File Size        : 3882528 bytes
+ *  Date Time        : 2025/04/09 18:29:38
+ *  Target           : Public
+ *  Cues             : 1
+ *  CueSheet Comment : 
+ *  Stream Awb Path  : C:/Users/s1len/OneDrive/Documents/CRIWARE/CriAtomCraft/projects/BuzzOrDie/Public/BGM/VS_Tutorial.awb
+ *===========================================================================*/
+
+#define CRI_VS_TUTORIAL_CUENUM (1)
+
+/* AISAC Control List (AISAC Control ID) */
+
+/* Cue List (Cue ID) */
+#define CRI_VS_TUTORIAL_CUE_0                ( 0) /*  */
+
+/* Block List (Block Index) */
+
+/* end of file */
+
